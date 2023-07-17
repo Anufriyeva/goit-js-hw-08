bfbd6fdb0d30cd709a05150cc75743c9aee94c96
Evgeniya Anufriyeva <2281474@gmail.com>
2023-07-17 15:46:55 +0300

    gallery-commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,4 +4,30 @@ import { galleryItems } from './gallery-items';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
+const galleryElement = document.querySelector('.gallery');
+
+function createElementGallery(items) {
+  return items.map(({ original, preview, description }) => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}">
+      </a>
+    </li>
+  `).join('');
+}
+
+const galleryMarkup = createElementGallery(galleryItems);
+galleryElement.insertAdjacentHTML('beforeend', galleryMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Space') {
+    lightbox.close();
+  }
+});
+
 console.log(galleryItems);
