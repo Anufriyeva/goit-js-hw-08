848f618ccde2b5a9bdd09499da02e29928e48b8b
Evgeniya Anufriyeva <2281474@gmail.com>
2023-07-17 16:29:04 +0300

    video-commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+
+function saveCurrentTime() {
+  player.getCurrentTime().then((currentTime) => {
+    localStorage.setItem('videoplayer-current-time', currentTime);
+  });
+}
+
+const throttledSaveCurrentTime = throttle(saveCurrentTime, 1000);
+
+player.on('timeupdate', throttledSaveCurrentTime);
+
+window.addEventListener('load', () => {
+  const currentTime = localStorage.getItem('videoplayer-current-time');
+  if (currentTime) {
+    player.setCurrentTime(parseFloat(currentTime));
+  }
+});
